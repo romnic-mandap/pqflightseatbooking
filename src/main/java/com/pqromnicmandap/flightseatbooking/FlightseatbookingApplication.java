02c27dfbd413c5d5915e6fafbf9207f02a9e6cee
Romar Nicholas G. Mandap <romnic.mandap@gmail.com>
2026-04-29 18:59:06 +0800

    init proj

---
+++ src/main/java/com/pqromnicmandap/flightseatbooking/FlightseatbookingApplication.java
@@ -0,0 +1,13 @@
+package com.pqromnicmandap.flightseatbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightseatbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightseatbookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pqromnicmandap/flightseatbooking/FlightseatbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pqromnicmandap.flightseatbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightseatbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
